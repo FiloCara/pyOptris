eb1c613c4b2f92d67667f1bc326d6ffb260fd3da
FiloCara <fili.cara@gmail.com>
2020-02-25 10:54:15 +0100

    add linux setup script

---
+++ libirimager_setup.sh
@@ -0,0 +1,28 @@
+echo "#########################################"
+echo "#########Setup Optris PI camera##########"
+echo "#########################################"
+
+#UCV configuration
+sudo bash -c 'echo "options uvcvideo nodrop=1" > /etc/modprobe.d/uvcvideo.conf'
+sudo rmmod uvcvideo; sudo modprobe uvcvideo nodrop=1
+
+if [[ $(command groups) != *"video"* ]]
+then
+    sudo usermod -a -G video $(whoami)
+
+fi
+
+# Install dependecies
+sudo apt-get install cmake freeglut3-dev libusb-1.0-0-dev
+
+# Install Libirimager
+mkdir ~/build_libirimager && cd ~/build_libirimager
+wget ftp.evocortex.com/libirimager-7.2.0-amd64.deb
+sudo dpkg -i libirimager-7.2.0-amd64.deb
+
+# Download calibration
+sudo ir_download_calibration
+
+# Copy calibration to current dir
+
+# cp ./python ~/
