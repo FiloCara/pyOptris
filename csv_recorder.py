1d5cd8549983c171acc71c317047e4efd0107f5a
FiloCara <fili.cara@gmail.com>
2020-02-25 10:26:03 +0100

    change code

--- csv_recorder.py
+++ csv_recorder.py
@@ -18,11 +18,16 @@ class _BaseRecorder:
         self.w, self.h = optris.get_thermal_image_size()
         print('{} x {}'.format(self.w, self.h)) 
 
+        assert (self.w > 0) and (self.h > 0), "Camera has not been detected"
+
         self._remove_garbage()
 
         self.results = []
 
     def _remove_garbage(self):
+        """
+        Remove first 300 frames from streaming, no idea way but they contains some garbage values
+        """
         i = 0
         while (i < 300):
             optris.get_thermal_image(self.w, self.h)
@@ -56,7 +61,7 @@ class CSVRecorder(_BaseRecorder):
         last_time = start_time
 
         while True:
-            if (time.time() - start_time) < timeout:
+            if round(time.time() - start_time) <= timeout:
                 tic = time.time()
                 # get thermal frame
                 frame = optris.get_thermal_image(self.w, self.h)
@@ -72,7 +77,8 @@ class CSVRecorder(_BaseRecorder):
     
     def save_csv(self, path="./"):
         for i, elem in enumerate(self.results):
-            np.savetxt(path + "thermal_IR_{}_{}.csv".format(i, round(elem['time'], 2)), (elem["frame"] - 1000.0) / 10.0, delimiter=';')
+            np.savetxt(path + "thermal_IR_{}_{}.csv".format(i, round(elem['time'], 2)),
+                 (elem["frame"] - 1000.0) / 10.0, delimiter=';', fmt='%1.2f')
 
 # TODO: to be finished
 class TCPRecorder(_BaseRecorder):
@@ -81,6 +87,7 @@ class TCPRecorder(_BaseRecorder):
         super().__init__(xml_path)
 
         self.ip = ip
+        # Define a TCP server
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     def run(self):
@@ -92,10 +99,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-w', "--wait", type=int, default=1)
-    parser.add_argument('-to', '--timeout', type=int, default=60)
+    parser.add_argument('-to', '--timeout', type=int, default=10)
+    parser.add_argument('-p', '--path', type=str, default="./images/")
 
     args = parser.parse_args()
 
     instance = CSVRecorder("./config_file.xml")
     instance.grab_pictures(wait=args.wait, timeout=args.timeout)
-    instance.save_csv("./test/")
+    instance.save_csv(path=args.path)
--- example.py
+++ example.py
@@ -5,13 +5,13 @@ optris.usb_init('config_file.xml')
 
 optris.set_palette(9)
 
-w, h = optris.get_thermal_image_size()
+w, h = optris.get_palette_image_size()
 print('{} x {}'.format(w, h))
 
 while True:
     frame = optris.get_palette_image(w, h)
     cv2.imshow('IR streaming', frame)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 optris.terminate()
